d9fd34e947dd687f7fd03789ce4033018a86cf00
eunho0922 <tylereunholee010203@gmail.com>
2023-03-28 19:57:49 +0900

    백준 11382 꼬마정민

---
+++ swiftAlgorithm/LittleJeongmin/main.swift
@@ -0,0 +1,16 @@
+//
+//  main.swift
+//  LittleJeongmin
+//
+//  Created by 이은호 on 2023/03/28.
+//
+
+import Foundation
+
+let arr = readLine()!.components(separatedBy: " ")
+
+var a = Int(arr[0])!
+var b = Int(arr[1])!
+var c = Int(arr[2])!
+
+print(a + b + c)
---
+++ swiftAlgorithm/swiftAlgorithm/main.swift
@@ -0,0 +1,11 @@
+//
+//  main.swift
+//  swiftAlgorithm
+//
+//  Created by 이은호 on 2023/03/28.
+//
+
+import Foundation
+
+print("Hello, World!")
+
