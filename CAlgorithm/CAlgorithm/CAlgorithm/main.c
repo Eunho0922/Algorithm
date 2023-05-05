a971a38022b6fae80c34914d9a18d3bdf6455a75
eunho0922 <tylereunholee010203@gmail.com>
2023-05-05 18:35:47 +0900

    백준 10171 C언어

--- CAlgorithm/CAlgorithm/CAlgorithm/main.c
+++ CAlgorithm/CAlgorithm/CAlgorithm/main.c
@@ -9,16 +9,10 @@
 
 int main()
 {
-    int n1, n2, result = 0, result1 = 0, result2 = 0, max = 0;
-    scanf("%d %d", &n1, &n2);
-    result = n1 * (n2 % 10);
-    result1 = n1 * ((n2 % 100) / 10);
-    result2 = n1 * (n2 / 100);
-    max = (result2 * 100) + (result1 * 10) + (result);
-    printf("%d\n", result);
-    printf("%d\n", result1);
-    printf("%d\n", result2);
-    printf("%d\n", max);
-    
+    printf("\\    /\\\n");
+    printf(" )  ( ')\n");
+    printf("(  /  ) \n");
+    printf(" \\(__)|\n");
     return 0;
 }
+    
