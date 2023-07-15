e47b80ad7ec9cc0440c2648b4e754070c009d3d3
은호 <127753071+Eunho0922@users.noreply.github.com>
2023-07-15 09:31:03 +0900

    25314

--- CTest/CTest/25304.c
+++ CTest/CTest/25304.c
@@ -1,15 +1,15 @@
-#include <stdio.h>
-
-int main(void)
-{
-    int result, ex_result = 0, count, n1, n2;
-    scanf("%d", &result);
-    scanf("%d", &count);
-    for (int i = count ; i > 0 ; i--) {
-        scanf("%d %d", &n1, &n2);
-        ex_result += (n1 * n2);
-    }
-    if (ex_result == result) { printf("Yes"); }
-    else { printf("No"); }
-    return 0;
-}
+//#include <stdio.h>
+//
+//int main(void)
+//{
+//    int result, ex_result = 0, count, n1, n2;
+//    scanf("%d", &result);
+//    scanf("%d", &count);
+//    for (int i = count ; i > 0 ; i--) {
+//        scanf("%d %d", &n1, &n2);
+//        ex_result += (n1 * n2);
+//    }
+//    if (ex_result == result) { printf("Yes"); }
+//    else { printf("No"); }
+//    return 0;
+//}
