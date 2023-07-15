f485714f46e0d5c06d91fca8be7c66fb0cf49713
은호 <127753071+Eunho0922@users.noreply.github.com>
2023-07-15 09:13:02 +0900

    8593

---
+++ CTest/CTest/10950.c
@@ -0,0 +1,12 @@
+//#include <stdio.h>
+//
+//int main(void)
+//{
+//    int count, a, b;
+//    scanf("%d", &count);
+//    
+//    for (int i = count; i > 0; i--) {
+//        scanf("%d %d", &a, &b);
+//        printf("%d\n", a + b);
+//    }
+//}
