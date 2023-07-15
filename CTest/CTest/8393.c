c0eff569593744c2b001e75f2fe05b5cac7b1bba
은호 <127753071+Eunho0922@users.noreply.github.com>
2023-07-15 09:27:15 +0900

    25304

---
+++ CTest/CTest/8393.c
@@ -0,0 +1,13 @@
+//#include <stdio.h>
+//
+//int main(void)
+//{
+//    int num, result = 0;
+//    scanf("%d", &num);
+//
+//    for (int i = num; i > 0; i--) {
+//        result += i;
+//    }
+//    printf("%d", result);
+//    return 0;
+//}
