7be0b70a8af403e4da47bcf18036f71b5c1f4608
은호 <127753071+Eunho0922@users.noreply.github.com>
2023-07-16 15:49:28 +0900

    10813

---
+++ CTest/10813.h
@@ -0,0 +1,13 @@
+//
+//  10813.h
+//  CTest
+//
+//  Created by 이은호 on 2023/07/16.
+//
+
+#ifndef _0813_h
+#define _0813_h
+
+#include <stdio.h>
+
+#endif /* _0813_h */
--- CTest/CTest/10810.c
+++ CTest/CTest/10810.c
@@ -1,17 +1,17 @@
-#include <stdio.h>
-
-int main(void)
-{
-    int N, M, a, b, c;
-    scanf("%d %d", &N, &M);
-    int arr[100] = { 0 };
-    for (int i = 0; i < M; i++) {
-        scanf("%d %d %d", &a, &b, &c);
-        for (int j = (a - 1); j <= (b - 1); j++) {
-            arr[j] = c;
-        }
-    }
-    for (int k = 0; k < N; k++) {
-        printf("%d ", arr[k]);
-    }
-}
+//#include <stdio.h>
+//
+//int main(void)
+//{
+//    int N, M, a, b, c;
+//    scanf("%d %d", &N, &M);
+//    int arr[100] = { 0 };
+//    for (int i = 0; i < M; i++) {
+//        scanf("%d %d %d", &a, &b, &c);
+//        for (int j = (a - 1); j <= (b - 1); j++) {
+//            arr[j] = c;
+//        }
+//    }
+//    for (int k = 0; k < N; k++) {
+//        printf("%d ", arr[k]);
+//    }
+//}
