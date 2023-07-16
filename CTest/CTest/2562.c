e8faba330de7408ff6450c7e13a973026af75822
은호 <127753071+Eunho0922@users.noreply.github.com>
2023-07-16 15:20:44 +0900

    2562

---
+++ CTest/CTest/2562.c
@@ -0,0 +1,25 @@
+
+#include <stdio.h>
+
+int main(void)
+{
+    int max = 0,count = 0;
+    int arr[9] = { 0 };
+    for (int i = 0; i < 9; i++) {
+        scanf("%d", &arr[i]);
+    }
+    max = arr[0];
+    for (int i = 0; i < 9; i++) {
+        if ( arr[i] >= max)
+        {
+            max = arr[i];
+        } else {
+            arr[i] = 0;
+        }
+    }
+    for (int i = 0; i < 9; i++) {
+        if ( arr[i] != 0 ) { count = i + 1; }
+    }
+    printf("%d\n%d", max, count);
+    return 0;
+}
