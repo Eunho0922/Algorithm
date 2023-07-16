cdab758cc94d17b94f5cd002487edd58d10a18a7
은호 <127753071+Eunho0922@users.noreply.github.com>
2023-07-16 15:15:53 +0900

    10818

---
+++ CTest/10818.h
@@ -0,0 +1,13 @@
+//
+//  10818.h
+//  CTest
+//
+//  Created by 이은호 on 2023/07/16.
+//
+
+#ifndef _0818_h
+#define _0818_h
+
+#include <stdio.h>
+
+#endif /* _0818_h */
---
+++ CTest/CTest/10818.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int a, max = 0, min = 0;
+    scanf("%d", &a);
+    int arr[a];
+    for (int i = 0; i < a; i++) {
+        scanf("%d", &arr[i]);
+    }
+    max = arr[0];
+    min = arr[0];
+    for (int i = 0; i < a; i++) {
+        if ( arr[i] >= max) { max = arr[i]; }
+        if ( arr[i] <= min) { min = arr[i]; }
+    }
+    printf("%d %d", min, max);
+    return 0;
+}
