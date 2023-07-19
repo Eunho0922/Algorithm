749533db6c555e8d039c74356a6a8b5d10cc311a
s <tylereunholee010203@gmail.com>
2023-07-19 20:05:18 +0900

    3052

--- CTest/CTest/3052.c
+++ CTest/CTest/3052.c
@@ -1,8 +1,28 @@
-//
-//  3052.c
-//  CTest
-//
-//  Created by 이은호 on 2023/07/16.
-//
+#include <stdio.h>
 
-#include "3052.h"
+int main(void)
+{
+    int count = 10, count1;
+    int arr[10];
+    for (int i = 0; i < 10; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    for (int j = 0; j < 10; j++) {
+        arr[j] = arr[j] % 42;
+        count = arr[j];
+    }
+
+    for (int i = 0; i < 10; i++) {
+        for (int j = 1; j < 10; j++) {
+            if (arr[i] == arr[j]) { arr[i] = 0; }
+        }
+    }
+
+    for (int i = 0; i < 10; i++) {
+        if (arr[i] == 0) { count--; }
+    }
+
+    printf("%d", count);
+    return 0;
+}
