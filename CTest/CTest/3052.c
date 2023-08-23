6b0869083a914b16a354f5dcf60b1790ad0e1844
은호 <127753071+Eunho0922@users.noreply.github.com>
2023-08-23 22:44:35 +0900

    자료구조 스타트

--- CTest/CTest/3052.c
+++ CTest/CTest/3052.c
@@ -1,46 +1,66 @@
+//#include <stdio.h>
+//
+//int main(void)
+//{
+//    int count = 10, count1;
+//    int arr[10];
+//    for (int i = 0; i < 10; i++) {
+//        scanf("%d", &arr[i]);
+//    }
+//
+//    for (int j = 0; j < 10; j++) {
+//        arr[j] = arr[j] % 42;
+//        count = arr[j];
+//    }
+//
+//    for (int i = 0; i < 10; i++) {
+//        for (int j = 1; j < 10; j++) {
+//            if (arr[i] == arr[j]) { arr[i] = 0; }
+//        }
+//    }
+//
+//    for (int i = 0; i < 10; i++) {
+//        if (arr[i] == 0) { count--; }
+//    }
+//
+//    printf("%d", count);
+//    return 0;
+//
+//
+//
+//    for (int i = 0; i < 10; i++) {
+//        if (arr[i] == 0) { count--; }
+//    }
+//    for (int i = 0; i < 10; i++) {
+//        if (arr[i] == 0) { count--; }
+//    }
+//
+//
+//    for (int i = 0; i < 10; i++) {
+//        if (arr[i] == 0) { count--; }
+//    }
+//    for (int i = 0; i < 10; i++) {
+//        if (arr[i] == 0) { count--; }
+//    }
+//
+//}
+
+
 #include <stdio.h>
+
+int score[10];
+int max;
 
 int main(void)
 {
-    int count = 10, count1;
-    int arr[10];
     for (int i = 0; i < 10; i++) {
-        scanf("%d", &arr[i]);
+        scanf("%d", &score[i]);
     }
-
-    for (int j = 0; j < 10; j++) {
-        arr[j] = arr[j] % 42;
-        count = arr[j];
-    }
-
+    max = score[0];
     for (int i = 0; i < 10; i++) {
-        for (int j = 1; j < 10; j++) {
-            if (arr[i] == arr[j]) { arr[i] = 0; }
+        if (score[i] >= max) {
+            max = score[i];
         }
     }
-
-    for (int i = 0; i < 10; i++) {
-        if (arr[i] == 0) { count--; }
-    }
-
-    printf("%d", count);
-    return 0;
-    
-    
-
-    for (int i = 0; i < 10; i++) {
-        if (arr[i] == 0) { count--; }
-    }
-    for (int i = 0; i < 10; i++) {
-        if (arr[i] == 0) { count--; }
-    }
-
-    
-    for (int i = 0; i < 10; i++) {
-        if (arr[i] == 0) { count--; }
-    }
-    for (int i = 0; i < 10; i++) {
-        if (arr[i] == 0) { count--; }
-    }
-
+    printf("%d", max);
 }
