a9484fa4eaca2d006b7944f74ed9b3fba91e637e
은호 <127753071+Eunho0922@users.noreply.github.com>
2023-10-26 21:24:36 +0900

    Swift 복습 중

--- swiftAlgorithm/SwiftGrammer/main.swift
+++ swiftAlgorithm/SwiftGrammer/main.swift
@@ -5,7 +5,60 @@
 //  Created by 이은호 on 2023/09/22.
 //
 
-import Foundation
+//import Foundation
+//
+//print("Hello, World!")
 
-print("Hello, World!")
+//import Foundation
+//
+//var name: String = "Eunho"
+//var age: Int = 17
+//var job = "iOS Programmer"
+//var height = 190.5
+//
+//print("\(type(of: height))")
+//age = 99
+//job = "writer"
+//print("저의 이름은 \(name)이고, 나이는 \(age)이며, 직업은 \(job)입니다. 비밀이지만, 키는 \(height)입니다.")
 
+//import Foundation
+//
+//print("Hello, World!")
+//
+//import Foundation
+//
+//let name: String = "Eunho"
+//var age: Int = 17
+//var job = "iOS Programmer"
+//let height = 190.5
+//
+//print("\(type(of: height))")
+//age = 99
+//job = "writer"
+//print("저의 이름은 \(name)이고, 나이는 \(age)이며, 직업은 \(job)입니다. 비밀이지만, 키는 \(height)입니다.")
+
+//import Foundation
+
+//var inteeger: Int = -100
+//let unsigendInteeger: UInt = 50
+//print("inteeger 값 :  \(inteeger), unsigendInteeger 값 : \(unsigendInteeger)")
+//print("Int 최댓값 : \(Int.max), Int 최솟값 : \(Int.min)")
+//print("UInt 최댓값 : \(UInt.max), UInt 최솟값 : \(UInt.min)")
+
+//import Foundation
+//
+//let decimalInteger: Int = 28
+//let binaryInteger: Int = 0b11100
+//let octalInteger: Int = 0o34
+//let hexadecimalInteger: Int = 0x1C
+//
+//print(decimalInteger, binaryInteger, octalInteger, hexadecimalInteger)
+
+//import Foundation
+//
+//var boolean: Bool = true
+//
+//boolean.toggle()
+//let iLoveYou: Bool = true
+//let isTimeUnlimited: Bool = false
+//print("시간은 무한입니까? : \(isTimeUnlimited)")
