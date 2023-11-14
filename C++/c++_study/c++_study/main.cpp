1ec0dcdd378c97fbefa8b240e3f23372eb3a2fbc
은호 <127753071+Eunho0922@users.noreply.github.com>
2023-11-14 19:46:40 +0900

    이중 연결 리스트

--- C++/c++_study/c++_study/main.cpp
+++ C++/c++_study/c++_study/main.cpp
@@ -116,44 +116,101 @@
 //    cout << result;
 //}
 
-#include <bits/stdc++.h>
-using namespace std;
+//#include <bits/stdc++.h>
+//using namespace std;
+//
+//int main(void) {
+//    ios_base::sync_with_stdio();
+//    cin.tie(NULL);
+//    
+//    stack<char> stk;
+//    
+//    int T;
+//    cin >> T;
+//    
+//    
+//    for (int i = 0; i < T; i++) {
+//        bool VSP = true;
+//        
+//        string str;
+//        cin >> str;
+//        
+//        for (int j = 0;j < str.length(); j++) {
+//            if (str[j] == '(') {
+//                stk.push(str[j]);
+//            } else if (!stk.empty() && str[j] == ')' && stk.top() == '(') {
+//                stk.pop();
+//            } else {
+//                VSP = false;
+//                break;
+//            }
+//        }
+//        
+//        if (!stk.empty()) {
+//            VSP = false;
+//        }
+//        
+//        if (VSP == false) {
+//            cout << "NO\n";
+//        } else {
+//            cout << "YES\n";
+//        }
+//    }
+//}
 
-int main(void) {
-    ios_base::sync_with_stdio();
-    cin.tie(NULL);
-    
-    stack<char> stk;
-    
-    int T;
-    cin >> T;
-    
-    
-    for (int i = 0; i < T; i++) {
-        bool VSP = true;
-        
-        string str;
-        cin >> str;
-        
-        for (int j = 0;j < str.length(); j++) {
-            if (str[j] == '(') {
-                stk.push(str[j]);
-            } else if (!stk.empty() && str[j] == ')' && stk.top() == '(') {
-                stk.pop();
-            } else {
-                VSP = false;
-                break;
-            }
-        }
-        
-        if (!stk.empty()) {
-            VSP = false;
-        }
-        
-        if (VSP == false) {
-            cout << "NO\n";
-        } else {
-            cout << "YES\n";
-        }
-    }
-}
+// ------------------- 이중 연결 리스트
+//#include <bits/stdc++.h>
+//using namespace std;
+//
+//int main(void) {
+//    ios_base::sync_with_stdio(false);
+//    cin.tie(NULL);
+//    
+//    queue<int>(que);
+//    
+//    int count;
+//    cin >> count;
+//    
+//    for (int i = 0; i < count; i++) {
+//        string command;
+//        cin >> command;
+//   
+//        if (command == "push") {
+//            int value;
+//            cin >> value;
+//            que.push(value);
+//        } else if (command == "pop") {
+//            if (que.empty()) 
+//            { 
+//                cout << "-1\n";
+//            } else {
+//                cout << que.front() << "\n";
+//                que.pop();
+//            }
+//        } else if (command == "size") {
+//            cout << que.size() << "\n";
+//        } else if (command == "empty") {
+//            if (que.empty()) {
+//                cout << "1\n";
+//            } else {
+//                cout << "0\n";
+//            }
+//        } else if (command == "front") {
+//            if (que.empty())
+//            {
+//                cout << "-1\n";
+//            } else {
+//                cout << que.front() << "\n";
+//            }
+//        } else if (command == "back") {
+//            if (que.empty())
+//            {
+//                cout << "-1\n";
+//            } else {
+//                cout << que.back() << "\n";
+//            }
+//        } else {
+//            return 0;
+//        }
+//    }
+//}
--- C_Study/C_Study/이중 연결 리스트.c
+++ C_Study/C_Study/이중 연결 리스트.c
@@ -85,63 +85,63 @@
 //    return 0;
 //}
 
-#include<stdio.h>
-#include<stdlib.h>
-
-
-struct Node {
-    struct Node* llink;
-    int data;
-    struct Node* rlink;
-};
-
-struct Node* head;
-
-void init(void) {
-    head->llink = head;
-    head->rlink = head;
-}
-
-void insert(struct Node* before, int value) {
-    struct Node* new = (struct Node*)malloc(sizeof(struct Node));
-    new->data = value;
-    
-    new->rlink = before->rlink;
-    new->llink = before;
-    before->rlink->llink = new;
-    before->rlink = new;
-}
-
-void delete(struct Node* removed) {
-    if (removed == head) {
-        return;
-    }
-    removed->llink->rlink = removed->rlink;
-    removed->rlink->llink = removed->llink;
-}
-
-void print_list(void) {
-    for (struct Node* p = head->rlink; p != head; p = p->rlink) {
-        printf("<- %d -> ", p->data);
-    }
-    printf("\n");
-}
-
-int main(void) {
-    head = (struct Node*)malloc(sizeof(struct Node));
-    init();
-    printf("삽입 단계\n");
-    for (int i = 0; i < 5; i++) {
-        insert(head, i);
-        print_list();
-    }
-    
-
-    printf("\n삭제 단계\n");
-    for (int i = 0; i < 5; i++) {
-        delete(head->rlink);
-        print_list();
-    }
-
-    return 0;
-}
+//#include<stdio.h>
+//#include<stdlib.h>
+//
+//
+//struct Node {
+//    struct Node* llink;
+//    int data;
+//    struct Node* rlink;
+//};
+//
+//struct Node* head;
+//
+//void init(void) {
+//    head->llink = head;
+//    head->rlink = head;
+//}
+//
+//void insert(struct Node* before, int value) {
+//    struct Node* new = (struct Node*)malloc(sizeof(struct Node));
+//    new->data = value;
+//    
+//    new->rlink = before->rlink;
+//    new->llink = before;
+//    before->rlink->llink = new;
+//    before->rlink = new;
+//}
+//
+//void delete(struct Node* removed) {
+//    if (removed == head) {
+//        return;
+//    }
+//    removed->llink->rlink = removed->rlink;
+//    removed->rlink->llink = removed->llink;
+//}
+//
+//void print_list(void) {
+//    for (struct Node* p = head->rlink; p != head; p = p->rlink) {
+//        printf("<- %d -> ", p->data);
+//    }
+//    printf("\n");
+//}
+//
+//int main(void) {
+//    head = (struct Node*)malloc(sizeof(struct Node));
+//    init();
+//    printf("삽입 단계\n");
+//    for (int i = 0; i < 5; i++) {
+//        insert(head, i);
+//        print_list();
+//    }
+//    
+//
+//    printf("\n삭제 단계\n");
+//    for (int i = 0; i < 5; i++) {
+//        delete(head->rlink);
+//        print_list();
+//    }
+//
+//    return 0;
+//}
